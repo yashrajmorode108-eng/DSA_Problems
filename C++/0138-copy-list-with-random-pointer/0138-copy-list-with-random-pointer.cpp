c001da4df40cfc5c90551ca4b6d21829020f2f9f
Yashraj Morode <yashrajmorode108@gmail.com>
2026-08-13 06:11:02 +0530

    Time: 3 ms (91.43%), Space: 15 MB (71.65%) - LeetHub

--- C++/0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ C++/0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -13,26 +13,51 @@ public:
     }
 };
 */
-
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-       unordered_map<Node*, Node*> mpp;
-       Node* temp = head;
+        if (head == nullptr)
+            return nullptr;
 
-       while(temp!= NULL){
-        mpp[temp] = new Node(temp->val);
-        temp= temp->next;
-       }
-       
-       temp = head;
+        Node* cur = head;
 
-       while(temp!= NULL){
-        mpp[temp]->next = temp->next ? mpp[temp->next]:nullptr;
-        mpp[temp]->random = temp->random ? mpp[temp->random]: nullptr;
-        temp = temp->next;
-       }
+        // Step 1: Insert copied nodes after original nodes
+        while (cur != nullptr) {
+            Node* copy = new Node(cur->val);
 
-       return mpp[head];
+            copy->next = cur->next;
+            cur->next = copy;
+
+            cur = copy->next;
+        }
+
+        // Step 2: Assign random pointers
+        cur = head;
+
+        while (cur != nullptr) {
+            Node* copy = cur->next;
+
+            if (cur->random != nullptr)
+                copy->random = cur->random->next;
+
+            cur = copy->next;
+        }
+
+        // Step 3: Separate original and copied lists
+        cur = head;
+        Node* copiedHead = head->next;
+
+        while (cur != nullptr) {
+            Node* copy = cur->next;
+
+            cur->next = copy->next;
+
+            if (copy->next != nullptr)
+                copy->next = copy->next->next;
+
+            cur = cur->next;
+        }
+
+        return copiedHead;
     }
 };
