97488df1d399fbe425707a66121f5f16c4407738
Yashraj Morode <yashrajmorode108@gmail.com>
2026-08-14 22:15:19 +0530

    Time: 57 ms (70.4%), Space: 70.9 MB (86.48%) - LeetHub

---
+++ C++/0912-sort-an-array/0912-sort-an-array.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    void heapify(vector<int>& nums, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && nums[left] > nums[largest])
+            largest = left;
+
+        if (right < n && nums[right] > nums[largest])
+            largest = right;
+
+        if (largest != i) {
+            swap(nums[i], nums[largest]);
+            heapify(nums, n, largest);
+        }
+    }
+
+    void heapSort(vector<int>& nums) {
+        int n = nums.size();
+
+        // Build max heap
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(nums, n, i);
+        }
+
+        // Move maximum element to the end one by one
+        for (int i = n - 1; i > 0; i--) {
+            swap(nums[0], nums[i]);
+            heapify(nums, i, 0);
+        }
+    }
+
+    vector<int> sortArray(vector<int> &nums){
+        heapSort(nums);
+        return nums;
+    }
+};
