fb92bba39022149da2769b536406fd4a0abd2f24
Yashraj Morode <yashrajmorode108@gmail.com>
2026-08-18 19:32:13 +0530

    Time: 5 ms (40.6%), Space: 29.1 MB (73.68%) - LeetHub

--- C++/3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
+++ C++/3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
@@ -2,45 +2,35 @@ class Solution {
 public:
     int largestInteger(vector<int>& nums, int k) {
         int n = nums.size();
-        unordered_map<int, vector<int>> pos;
 
-        for (int i = 0; i < n; i++)
-            pos[nums[i]].push_back(i);
+        vector<int> freq(51, 0);
 
-        int ans = -1;
-        int maxStart = n - k;
+        for (int x : nums) {
+            freq[x]++;
+        }
+        if (k == n) {
+            return *max_element(nums.begin(), nums.end());
+        }
+        if (k == 1) {
+            int ans = -1;
 
-        for (auto &entry : pos) {
-            auto &v = entry.second;
-
-            int covered = 0;
-            bool has = false;
-            int curL = 0, curR = -1;
-
-            for (int idx : v) {
-                int L = max(0, idx - k + 1);
-                int R = min(idx, maxStart);
-
-                if (L > R) continue; // occurrence is in no k-window
-
-                if (!has) {
-                    curL = L;
-                    curR = R;
-                    has = true;
-                } else if (L <= curR + 1) {
-                    curR = max(curR, R);
-                } else {
-                    covered += curR - curL + 1;
-                    curL = L;
-                    curR = R;
+            for (int x : nums) {
+                if (freq[x] == 1) {
+                    ans = max(ans, x);
                 }
             }
 
-            if (has)
-                covered += curR - curL + 1;
+            return ans;
+        }
 
-            if (covered == 1)
-                ans = max(ans, entry.first);
+        int ans = -1;
+
+        if (freq[nums[0]] == 1) {
+            ans = max(ans, nums[0]);
+        }
+
+        if (freq[nums[n - 1]] == 1) {
+            ans = max(ans, nums[n - 1]);
         }
 
         return ans;
